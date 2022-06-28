9c600a23242ff399a6362622cf4647e7fafa7ba0
econdami <econdami@yahoo.fr>
2022-06-28 18:01:17 +0200

    work on the nipype Smooth with capsul V3

--- python/mia_processes_3.0/tests.py
+++ python/mia_processes_3.0/tests.py
@@ -4,33 +4,69 @@ import typing
 
 
 class Smooth(Process):
+
+ #   def __init__(self):
+  #      self.requirement = ['spm', 'nipype']
+
+    #We can't use types defined by nipype because it use traits ...
+    #from nipype.interfaces.base import (File, InputMultiPath, InputMultiObject,
+    #                                    OutputMultiPath, traits,
+    #                                    TraitListObject,
+    #                                    traits_extension)
+    #from nipype.interfaces.spm.base import ImageFileSPM
+
+    #in_files: InputMultiPath[ImageFileSPM] = field(extensions='.nii',
+    #                                               optional=False,
+    #                                               doc='in_files')
     # Inputs
-    in_files: field(type_=File,
-                    extensions=('.nii',),
-                    optional=False,
-                    doc='in_files')
-    
-    fwhm: field(type_=typing.Union[float, list[float]],
-                optional=True,
-                doc='fwhm')
+    in_files: File = field(extensions='.nii',
+                           optional=False,
+                           doc='in_files description')
 
-    data_type: field(type_=int,
-                     optional=True,
-                     doc='data_type')
+    fwhm: typing.Union[float, list[float]] = field(optional=True,
+                                                   default_factory=lambda: [6., 6., 6.],
+                                                   doc='fwhm description')
 
-    implicit_masking: field(type_=bool,
-                            optional=True,
-                            doc='implicit_masking')
+    data_type: int = field(default_factory=lambda: 0,
+                           optional=True,
+                           doc='data_type')
 
-    out_prefix: field(type_=str,
-                      optional=True,
-                      default_factory=lambda: 's',
-                      doc='out_prefix')
+    implicit_masking: bool = field(optional=True,
+                                   default_factory=lambda: False,
+                                   doc='implicit_masking description')
+
+    out_prefix: str = field(optional=True,
+                            default_factory=lambda: 's',
+                            doc='out_prefix description')
 
         # Outputs
-    smoothed_files: field(type_=File,
-                          write=True,
-                          doc='smoothed_files')
+    smoothed_files: File = field(write=True,
+                                 doc='smoothed_files description')
+
+    capsul = Capsul()
+    process = capsul.executable('nipype.interfaces.spm.preprocess.Smooth')
+
+    def execute(self, context):
+        # with open(self.input) as f:
+        #     content = f.read()
+        # content = f'{content}Bias correction with strength={self.strength}\n'
+        # Path(self.output).parent.mkdir(parents=True, exist_ok=True)
+        # with open(self.output, 'w') as f:
+        #     f.write(content)
+        print('self.in_files: ', self.in_files)
+        print('self.fwhm: ', self.fwhm)
+        print('self.data_type: ', self.data_type)
+        print('self.implicit_masking: ', self.implicit_masking)
+        print('self.out_prefix: ', self.out_prefix)
+        self.process.in_files = self.in_files
+        self.process.fwhm= self.fwhm
+        self.process.data_type = self.data_type
+        self.process.implicit_masking = self.implicit_masking
+        self.process.out_prefix = self.out_prefix
+        self.process.output_directory = self.output_directory
+
+        #print('self.smoothed_files: ', self.smoothed_files)
+        print('self.process: ', self.process)
 
 
 
@@ -39,12 +75,55 @@ if __name__ == '__main__':
     import sys
     from soma.qt_gui.qt_backend import QtGui
     from capsul.qt_gui.widgets import PipelineDeveloperView
-    smooth = Capsul.executable('test.Smooth')
+    from capsul.config.configuration import ApplicationConfiguration
+
     app = QtGui.QApplication.instance()
+
+    capsul = Capsul()
+    smooth = capsul.executable('tests.Smooth')
+
+    config = ApplicationConfiguration('test_smoothV3')
+    user_conf_dict = {
+        'local': {
+            'spm': {
+                'spm12_standalone': {
+                    'directory': '/usr/local/SPM/spm12_standalone',
+                    'standalone': True,
+                    'version': 12},
+                    },
+            'nipype': {
+                'nipype': {}
+                      },
+            'matlab': {
+                'matlab': {
+                    'mcr_directory': '/usr/local/MATLAB/MATLAB_Runtime/v95 '}
+                      }
+                  }
+                }
+
+    config.user = user_conf_dict
+    config.merge_configs()
+    capsul.config = config.merged_config
+    print('local config: ', capsul.config.asdict())
+
+    smooth.in_files = '/home/econdami/Desktop/Data_tests_capsulV3/raw/alej170316-IRM_Fonct._+_perfusion-2016-03-17_08-34-44-0-BOLD_CVR_7_53sl_ModeratePNS_SENSE-FEEPI-00-12-12.000.nii'
+    smooth.output_directory = '/home/econdami/Desktop/Data_tests_capsulV3/derived'
+
+
+    #c.engine().config.local ######  pas bon
+    #c.config.local= un dict ou un controller
+    #https://github.com/populse/capsul/blob/pydantic_controller/capsul/config/test/test_config.py
     if not app:
         app = QtGui.QApplication(sys.argv)
+        
     view1 = PipelineDeveloperView(smooth, allow_open_controller=True, show_sub_pipelines=True)
+    
     view1.show()
     app.exec_()
     del view1
 
+    
+    with capsul.engine() as engine:
+        engine.run(smooth)
+    
+    print('smooth.asdict(): ', smooth.asdict())
